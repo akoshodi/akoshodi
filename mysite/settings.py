fc143e2bf809c0db315f42b5179f89fde6de8074
Akinwale Oshodi <akoshodi@gmail.com>
2013-12-28 05:53:10 +0100

    Added Blog app

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,12 +31,18 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
+    # Default Apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Third Party Apps
+
+    # Local Apps
+    'blog',
 )
 
 MIDDLEWARE_CLASSES = (
